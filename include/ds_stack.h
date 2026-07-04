d4f83814274ffc4b98f5423aaccff7eb7851b5b7
Villy-P <valeriuspetrini@gmail.com>
2026-07-04 07:58:37 -0500

    feat: update ds_stack functions to return DS_STATUS for improved error handling

--- include/ds_stack.h
+++ include/ds_stack.h
@@ -14,11 +14,11 @@ typedef ds_array ds_stack;
         return (name*)ds_array_create(initial_capacity, sizeof(T), cmp_fn, destroy_fn); \
     } \
     \
-    static inline bool name##_init(name* stack, size_t initial_capacity, ds_cmp_fn cmp_fn, ds_destroy_fn destroy_fn) { \
+    static inline DS_STATUS name##_init(name* stack, size_t initial_capacity, ds_cmp_fn cmp_fn, ds_destroy_fn destroy_fn) { \
         return ds_array_init((ds_array*)stack, initial_capacity, sizeof(T), cmp_fn, destroy_fn); \
     } \
     \
-    static inline bool name##_init_default(name* stack) { \
+    static inline DS_STATUS name##_init_default(name* stack) { \
         return ds_array_init_default((ds_array*)stack, sizeof(T)); \
     } \
     \
@@ -30,11 +30,11 @@ typedef ds_array ds_stack;
         ds_array_destroy((ds_array*)stack); \
     } \
     \
-    static inline bool name##_push(name* stack, T value) { \
+    static inline DS_STATUS name##_push(name* stack, T value) { \
         return ds_array_push((ds_array*)stack, &value); \
     } \
     \
-    static inline bool name##_pop(name* stack, T* out) { \
+    static inline DS_STATUS name##_pop(name* stack, T* out) { \
         return ds_array_pop((ds_array*)stack, out); \
     } \
     \
@@ -49,11 +49,11 @@ static inline ds_stack* ds_stack_create(size_t initial_capacity, size_t member_s
     return ds_array_create(initial_capacity, member_size, cmp_fn, destroy_fn);
 }
 
-static inline bool ds_stack_init(ds_stack* stack, size_t initial_capacity, size_t member_size, ds_cmp_fn cmp_fn, ds_destroy_fn destroy_fn) {
+static inline DS_STATUS ds_stack_init(ds_stack* stack, size_t initial_capacity, size_t member_size, ds_cmp_fn cmp_fn, ds_destroy_fn destroy_fn) {
     return ds_array_init(stack, initial_capacity, member_size, cmp_fn, destroy_fn);
 }
 
-static inline bool ds_stack_init_default(ds_stack* stack, size_t member_size) {
+static inline DS_STATUS ds_stack_init_default(ds_stack* stack, size_t member_size) {
     return ds_array_init_default(stack, member_size);
 }
 
@@ -65,11 +65,11 @@ static inline void ds_stack_destroy(ds_stack* stack) {
     ds_array_destroy(stack);
 }
 
-static inline bool ds_stack_push(ds_stack* stack, const void* element) {
+static inline DS_STATUS ds_stack_push(ds_stack* stack, const void* element) {
     return ds_array_push(stack, element);
 }
 
-static inline bool ds_stack_pop(ds_stack* stack, void* out) {
+static inline DS_STATUS ds_stack_pop(ds_stack* stack, void* out) {
     return ds_array_pop(stack, out);
 }
 
@@ -80,7 +80,7 @@ static inline void* ds_stack_peek(ds_stack* stack) {
     return ds_array_get(stack, stack->length - 1);
 }
 
-static inline bool ds_stack_reverse(ds_stack* stack) {
+static inline DS_STATUS ds_stack_reverse(ds_stack* stack) {
     return ds_array_reverse(stack);
 }
 #endif
