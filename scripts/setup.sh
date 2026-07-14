69ead0fbdef90ee781c47c8fee4de2767c956c72
Valerius <valeriuspetrini@gmail.com>
2026-07-14 07:17:57 -0500

    chore(scripts): change pip to pipx

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 sudo apt update
-sudo apt install -y build-essential cmake git ninja-build doxygen python3-pip
+sudo apt install -y build-essential cmake git ninja-build doxygen pipx
+
+pipx ensurepath
+pipx install pre-commit
+
+echo "Setup Complete"
